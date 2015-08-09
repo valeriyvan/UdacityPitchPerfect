a5d20cda7888bcee425d4a505b297641db3f77fd
Valeriy Van <github@w7software.com>
2015-08-09 14:18:21 +0300

    ViewController's UI applied

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        recordButton.enabled = true
+        stopButton.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordButton.enabled = false
+        recordingInProgress.hidden = false
+        stopButton.hidden = false
+    }
 
+    @IBAction func stopAudio(sender: UIButton) {
+        recordingInProgress.hidden = true
+    }
 }
 
